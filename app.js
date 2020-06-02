dca96ba082efd3eec6122e41586996f1a0f10791
akshayx11 <akshayx11@gmail.com>
2020-06-03 00:34:13 +0530

    routes test commit 3

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-const toRouters = require('./textOperations');
-app.use('/', toRouters);
-
+//const toRouters = require('./textOperations');
+const toRouterPath = path.join(__dirname, '/textOperations');
+app.use(express.static(toRouterPath));
+app.use('/', toRouterPath);
 app.listen(process.env.port || 3000, (req, res) => {
     console.log("Server running!!");
 });
